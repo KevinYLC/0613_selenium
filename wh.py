d38b0c0d8b4203f6eec25cb3b626e83f0ef10028
Kevin <kevin.ntcse211@gmail.com>
2022-06-14 15:25:42 +0800

    test08

--- wh.py
+++ wh.py
@@ -1,6 +1,8 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import requests , json ,firebase_admin ,zlib,datetime
+from firebase_admin import credentials ,firestore
 
 options = webdriver.ChromeOptions()
 # options.add_argument("--start-maximized")
@@ -9,14 +11,20 @@ options.add_argument("--disable-popup-blocking ")
 options.add_argument('--window-size=1920,1080')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36")
 
+today = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8)))
+cred = credentials.Certificate('testkey.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+collection_ref = db.collection("test")
+
 driver = webdriver.Chrome(options = options)
 nd = webdriver.Chrome(options = options) 
 url = "https://www.accuweather.com/zh/tw/taiwan-weather"
 driver.get(url)
 time.sleep(5)
-
+doc_ref = db.collection("test").document("key")
+msg = ""
 for i in driver.find_elements_by_xpath("/html/body/div/div[4]/div[1]/div/div[2]/div/a"):
-    msg = ""  
     if i.find_element_by_class_name("text.title.no-wrap").text[2:] in ["市","縣"]:
         area = i.find_element_by_class_name("text.title.no-wrap").text
         whurl = i.get_attribute("href")
@@ -32,5 +40,5 @@ for i in driver.find_elements_by_xpath("/html/body/div/div[4]/div[1]/div/div[2]/
                 time.sleep(0.2)
             for a , b , c in zip(k.find_elements_by_xpath("//*/div[1]/div/div[1]/h2/span") , k.find_elements_by_class_name("phrase") ,k.find_elements_by_class_name("temp.metric")):
                 msg += f"{a.text} : {b.text} {c.text} \n"
-        print(msg)
+doc_ref.update({str(today.date()) : msg})
 quit()
